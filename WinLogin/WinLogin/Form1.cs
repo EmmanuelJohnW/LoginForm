c62ff476395d067027f736e3c3264d40297810ae
janem <134920676+EmmanuelJohnW@users.noreply.github.com>
2026-02-18 17:50:32 +0800

    LoginAppsdev V1
    version 1 ni siya

---
+++ WinLogin/WinLogin/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinLogin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_CLick_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "janem" && txtPassword.Text == "23207038")
+            {
+                frmMain main = new frmMain();
+                main.ShowDialog();
+            }
+            else
+            {
+                lblError.Text = "Invalid username and password.";
+            }
+        }
+    }
+}
---
+++ WinLogin/WinLogin/Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WinLogin
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.exercisesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercise1BAgsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exercise2ConvertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exercisesToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(524, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // exercisesToolStripMenuItem
+            // 
+            this.exercisesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exercise1BAgsToolStripMenuItem,
+            this.exercise2ConvertToolStripMenuItem});
+            this.exercisesToolStripMenuItem.Name = "exercisesToolStripMenuItem";
+            this.exercisesToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.exercisesToolStripMenuItem.Text = "Exercises";
+            // 
+            // exercise1BAgsToolStripMenuItem
+            // 
+            this.exercise1BAgsToolStripMenuItem.Name = "exercise1BAgsToolStripMenuItem";
+            this.exercise1BAgsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exercise1BAgsToolStripMenuItem.Text = "Exercise 1: Bags";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // exercise2ConvertToolStripMenuItem
+            // 
+            this.exercise2ConvertToolStripMenuItem.Name = "exercise2ConvertToolStripMenuItem";
+            this.exercise2ConvertToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exercise2ConvertToolStripMenuItem.Text = "Exercise 2: Convert";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 348);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Projects";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exercisesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercise1BAgsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exercise2ConvertToolStripMenuItem;
+    }
+}
